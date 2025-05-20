1607881a3691a3661de2782780977c1e17724a1f
fransgtg <fransmalindoginting@gmail.com>
2025-05-20 21:23:55 +0700

    latihan.cpp

--- pertemuan 3/latihan.cpp
+++ pertemuan 3/latihan.cpp
@@ -9,12 +9,12 @@ int main() {
     cin >> nilai;
 
     if (nilai % 10 == 0) {
-        cout << "Kelipatan 10";
+        cout << "Kelipatan 10"; //Kondisi ini terlebih dahulu agar inputan 20 tidak masuk ke kelipatan 5
     } else if (nilai % 5 == 0) {
         cout << "Kelipatan 5";
     } else {
         cout << "Error";
-    }
+    } //Mengecek bilangan apakah kelipatan 10 atau 5
 
     return 0;
 }
