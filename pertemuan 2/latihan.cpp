9a8d1f5d6fe44dfe4ab82bb2eccd12d2b31deb54
fransgtg <fransmalindoginting@gmail.com>
2025-05-20 16:10:00 +0700

    latihan.cpp

--- pertemuan 2/latihan.cpp
+++ pertemuan 2/latihan.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main () {
     system("cls");
     float luas,vol,r;
-    const float phi = 3.14;
+    const float phi = 3.14; //Konstanta(nilai yg tidak bisa diubah)
     cout<<"r = "; cin>>r;
 
     luas = 4 * phi * r * r;
-    vol = (float) 4/3 * phi * r * r * r;
+    vol = (float) 4/3 * phi * r * r * r; //Agar hasil yg ditampilkan desimal
 
     cout<<"Luas permukaan :" <<luas <<endl;
     cout<<"Volume : " <<vol;
