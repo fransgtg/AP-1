c56858f4c0aaa99cba24dba94901f025e6d1c6bd
fransgtg <fransmalindoginting@gmail.com>
2025-05-23 22:12:49 +0700

    latihan1.cpp

--- pertemuan 4/latihan1.cpp
+++ pertemuan 4/latihan1.cpp
@@ -8,8 +8,8 @@ int main () {
 
     cout<<"Masukkkan kalimat : "; getline(cin,kalimat);
 
-    for(i = 0; i < kalimat.length(); i++) {
-        kalimat[i] = toupper(kalimat[i]);
+    for(i = 0; i < kalimat.length(); i++) { //kalimat.length, menghitung jumlah karakter pada variabel kalimat
+        kalimat[i] = toupper(kalimat[i]); //toupper, mengubah huruf kecil menjadi huruf kapital pada indeks ke-i
     }
     cout<<"Kalimat dalam hururf kapital : " <<kalimat <<endl;
 
