851096c43ebfc71e796976a9e956b31c7c086a80
fransgtg <fransmalindoginting@gmail.com>
2025-05-23 22:41:59 +0700

    latihan3.cpp

--- pertemuan 4/latihan3.cpp
+++ pertemuan 4/latihan3.cpp
@@ -14,14 +14,14 @@ int main() {
     // Membuat string "expected" berdasarkan pola tertentu
     for (int i = 0; i < password.length(); i++) {
         expected += password.substr(0, i + 1);
-    }
+    } //password.substr(0, i+1), mengambil bagian dari string dari indeks ke-o sebanyak i + 1. Setelah itu disimpan di var expected dan diulang sebanyak banyak karakter pada password.
 
     // Mengecek apakah input kedua sesuai dengan pola yang diharapkan
     if (expected == sandi) {
         cout << "Tulisan agen benar" << endl;
     } else {
         cout << "Tulisan agen salah" << endl;
-    }
+    } //mengecek apakah input sandi sama dengan expected
 
     return 0;
 }
