d32510e60b354256e93c6c356d366a9420786b3a
fransgtg <fransmalindoginting@gmail.com>
2025-05-24 23:08:08 +0700

    vector.cpp

--- pertemuan 5/vector.cpp
+++ pertemuan 5/vector.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <vector> //library untuk vector
 using namespace std;
 
 int main() {
     system("cls");
     //Vector Declaration & Intialization
 
-    vector<string> nama_karyawan = {"Frans","Rifki","Fatih","Dapa"};
+    vector<string> nama_karyawan = {"Frans","Rifki","Fatih","Dapa"}; // vector sama seperti array namun sifatnya fleksibel (ukuranya bisa diubah)
     // for (string karyawan : nama_karyawan) {
-    //     cout<<karyawan <<endl;
+    //     cout<<karyawan <<endl; //mengubah nama variabel nama_karyawan menjadi karyawan
     // }
 
     //Add data to vector
-    nama_karyawan.push_back("Dhafa");
+    nama_karyawan.push_back("Dhafa"); // menambahkan "Dhafa" pada vector
     // for(int i = 0; i < nama_karyawan.size(); i++) {
-    //     cout<<nama_karyawan[i] <<endl;
+    //     cout<<nama_karyawan[i] <<endl; //menampilkan vector
     // }
 
     //Delete data from Vector
-    nama_karyawan.pop_back();
+    nama_karyawan.pop_back(); //menghapus elemen terakhir pada vector
 
-    nama_karyawan.erase(nama_karyawan.begin() + 3);
+    nama_karyawan.erase(nama_karyawan.begin() + 3); //menghapus elemen pada indeks ke-3 terakhir
 
     for(int i = 0; i < nama_karyawan.size(); i++) {
         cout<<nama_karyawan[i] <<endl;
-    }
+    } //"Dapa" dan ""Dhafa" terhapus
 }
