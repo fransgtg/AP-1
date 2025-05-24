a048ca3ad6e5b910d3126bd18866fe4cc99c821a
fransgtg <fransmalindoginting@gmail.com>
2025-05-24 23:18:41 +0700

    latihanArray.cpp

--- pertemuan 5/latihanArray.cpp
+++ pertemuan 5/latihanArray.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     system("cls");
-    // penjumlahan matriks 2 x 2
+    // penjumlahan matriks 2 x 2 //Deklarasi array untuk matriks 2x2
     int matriks1[2][2];
     int matriks2[2][2];
     int hasil[2][2];
@@ -14,7 +14,7 @@ int main() {
             cout<<"Masukkan elemen baris " <<i + 1 <<" kolom " <<j + 1 <<" : ";
             cin>>matriks1[i][j];
         }
-    }
+    } //Nested loop untuk input elemen-elemen matriks
     cout<<"Matriks 2 : " <<endl;
     for(int i = 0; i < 2; i++) {
         for(int j = 0; j < 2; j++) {
@@ -28,7 +28,7 @@ int main() {
         for(int j = 0; j < 2; j++) {
             hasil[i][j] = matriks1[i][j] + matriks2[i][j];
             cout<<hasil[i][j] <<" ";
-        }
+        } //meenjummlahkan elemen-elemen matriks
         cout<<endl;
     }
 }
