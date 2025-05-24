5f3d2fbf7b5b2dddda05c81d227c10e37d43d817
fransgtg <fransmalindoginting@gmail.com>
2025-05-24 23:40:12 +0700

    fungsi.cpp

--- pertemuan 6/fungsi.cpp
+++ pertemuan 6/fungsi.cpp
@@ -6,24 +6,24 @@ void tampilkanPesan() {
     cout<<"==SELAMAT DATANG DI AP 1==" <<endl;
 }
 
-//fungsi dengan nilai balikan
+//fungsi dengan nilai balikan, mengembalikan nilai ke pemanggil fungsi dengan return
 int tambah(int a, int b) {
     return a + b;
 }
 
-//fungsi Overload
+//fungsi Overload, mendefinisikan fungsi dengan nama yg sama, tetapi parameternya berbeda
 int kali(int a, int b) {
     return a * b;
 }
 
 double kali(double a, double b) {
     return a * b;
-}
+} // nama fungsi sama, tetapi parameter pada fungsi tersebut menggunakan tipe data yg berbeda
 
-//fungsi rekursif : mengitung nilai faktorial
+//fungsi rekursif : mengitung nilai faktorial //fungsi yg memanggil dirinya sendiri
 int faktorial(int n) {
     if(n == 0 || n == 1) {
-        return 1;
+        return 1; //faktorial dari 0 atau 1 = 1
     } else {
         return n * faktorial(n-1);
     }
@@ -32,7 +32,7 @@ int faktorial(int n) {
 int main() {
     system("cls");
 
-    //menggunakan fuungsi tanpa nilai balikan
+    //menggunakan fungsi tanpa nilai balikan
     tampilkanPesan();
 
     int x = 5, y = 3;
@@ -48,5 +48,5 @@ int main() {
 
     //menggunakan fungsi rekursif
     int angka = 5;
-    cout<<"Faktorial dari " <<angka <<" adalah " <<faktorial(angka) <<endl;
+    cout<<"Faktorial dari " <<angka <<" adalah " <<faktorial(angka) <<endl; //ketika 5 dikalikan maka fungsi tersebut memanggil fungsi faktorial dengan n-1. Begitu seterusnya sampai n = 1
 }
